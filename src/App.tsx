3387b381f9c9baa723d6ca30cc56de7d4de7f789
cros6y6raga <crosbybraga@gmail.com>
2023-01-05 12:48:00 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Count} from "./components/Count";
 
 function App() {
+    const [count, setCount] = useState(0)
+    const minValue = 0;
+    const maxValue = 10;
+    const onClickInc = () => {
+        setCount(count + 1)
+    }
+    const onClickReset = () => {
+        setCount(minValue)
+    }
     return (
-        <>
-        </>
+        <Count
+            count={count}
+            minValue={minValue}
+            maxValue={maxValue}
+            onClickInc={onClickInc}
+            onClickReset={onClickReset}
+        />
     );
 }
 
---
+++ src/components/Count.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type PropsType = {
+    count: number
+    onClickInc: () => void
+    onClickReset: () => void
+    minValue: number
+    maxValue: number
+}
+export const Count = (props: PropsType) => {
+    return (
+        <div className='counter'>
+            <div className='monitor'>
+                <span className={props.count === props.maxValue ? 'error' : ''}>{props.count}</span>
+            </div>
+            <div className='buttons'>
+                <button className='button' disabled={props.count === props.maxValue} onClick={props.onClickInc}>inc</button>
+                <button className='button' disabled={props.count === props.minValue} onClick={props.onClickReset}>reset</button>
+            </div>
+        </div>
+    );
+};
