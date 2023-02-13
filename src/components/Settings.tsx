73b4a6a91f0d60d0059ce9b52c6af2c8d2cbd856
cros6y6raga <crosbybraga@gmail.com>
2023-02-13 10:10:01 +0300

    Created a minimum and a start value

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 export const Settings = () => {
     return (
         <div className='settings'>
-
+            <div className='settingsValue'>
+                <div className='settingsMainSpan'>
+                    <span>max value: <input className='settingsInput' type="number"/></span>
+                    <span>start value: <input className='settingsInput' type="number"/></span>
+                </div>
+            </div>
         </div>
     );
 };
