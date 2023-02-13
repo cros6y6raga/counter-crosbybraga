b29bb9eeeb7288b3ddfe0a183ffb70c9419a5e28
cros6y6raga <crosbybraga@gmail.com>
2023-02-13 10:24:21 +0300

    Added set button

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -4,10 +4,11 @@ export const Settings = () => {
     return (
         <div className='settings'>
             <div className='settingsValue'>
-                <div className='settingsMainSpan'>
-                    <span>max value: <input className='settingsInput' type="number"/></span>
-                    <span>start value: <input className='settingsInput' type="number"/></span>
-                </div>
+                <span>max value: <input className='settingsInput' type="number"/></span>
+                <span>start value: <input className='settingsInput' type="number"/></span>
+            </div>
+            <div className='settingsButton'>
+                <button className='set'>set</button>
             </div>
         </div>
     );
